b59b6c8421079ca7b79dd97a44e15fd8f0587ae9
longchenggong <972150074@qq.com>
2020-06-09 14:46:21 +0800

    sss

--- oa/src/test/java/com/ello/oa/main.java
+++ oa/src/test/java/com/ello/oa/main.java
@@ -1,11 +1,24 @@
 package com.ello.oa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @create 2020/5/26  18:13 author：longchenggong
  */
 
 public class main {
     public static void main(String[] args) {
+
+
+        for (int i = 0; i <12 ; i++) {
+            System.out.println(i);
+        }
+
+        Map m=new HashMap();
+        m.put("2","dsf");
+        m.put("2","dsf");
+        m.put("2","dsf");
         System.out.println("sfsdf");
     }
 }
